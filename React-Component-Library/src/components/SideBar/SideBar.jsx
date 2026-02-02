afd3fc3e43abe61197641e5f3e4a16ea765ca217
Manoj Garje <manojgarje123@gmail.com>
2026-02-02 14:38:42 +0530

    Apply styling

--- React-Component-Library/src/components/SideBar/SideBar.jsx
+++ React-Component-Library/src/components/SideBar/SideBar.jsx
@@ -1,17 +1,21 @@
-const SideBar = () => {
+import React from 'react';  
+import './SideBar.css';
 
-    const[isOpen, setIsOpen] = useState(false);
-    
-    return (
-        <div className='sidebar'>
-            <div className="sidebar-trigger">
-                <button className='sidebar-open-button' onClick={() => setIsOpen(true)}>*</button>
-            </div>
+const SideBar = ({ isOpen, onOpen, onClose, children }) => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-open">
+        <button className='sidebar-open-button' onClick={onOpen}>*</button>
+      </div>
 
-            <div className={`sidebar ${isOpen ? 'sidebar-open' : 'sidebar-closed'}`}>
-                <button className='sidebar-close-button' onClick={() => setIsOpen(false)}>x</button>
-            </div>
-        </div>
-      
-    )
-}
+      <div className={`sidebar-panel ${isOpen ? 'open' : 'closed'}`}>
+        <button className="sidebar-close-button" onClick={onClose}>
+          x
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
