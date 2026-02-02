3760b945d02a54fe2e74076747b6ec650dd6abb1
Manoj Garje <manojgarje123@gmail.com>
2026-02-02 11:35:10 +0530

    Fixes : SideBar

--- React-Component-Library/src/components/SideBar/SideBar.jsx
+++ React-Component-Library/src/components/SideBar/SideBar.jsx
@@ -2,11 +2,11 @@ const SideBar = ({isOpen, onClose, onOpen, children}) => {
     return (
         <div>
             <div className="sidebar-trigger">
-            <button onClick={onOpen}>*</button>
+                <button onClick={onOpen}>*</button>
             </div>
 
             <div className={`sidebar ${isOpen ? 'sidebar-open' : 'sidebar-closed'}`}>
-                    <button className='sidebar-close-button' onClick={onClose}>x</button>
+                <button className='sidebar-close-button' onClick={onClose}>x</button>
             </div>
         </div>
       
