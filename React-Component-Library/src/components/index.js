cea5d7812c9dff148b7c04003951497a1aa97112
Manoj Garje <manojgarje123@gmail.com>
2026-02-05 12:02:22 +0530

    Adds : index.js at inner and root level

---
+++ React-Component-Library/src/components/index.js
@@ -0,0 +1,6 @@
+export { Banner } from './Banner/Banner.jsx';
+export { default as SideBar } from './SideBar/SideBar.jsx';
+export { TableGrid } from './TableGrid/TableGrid.jsx';
+export { default as TableHeader } from './TableGrid/TableHeader.jsx';
+export { default as TableRow } from './TableGrid/TableRow.jsx';
+export { TableAction } from './TableGrid/TableAction.jsx';
